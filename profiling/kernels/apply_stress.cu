bb81a2c913251b217d41fcee6a8979e7cbd521eb
Matthew Mitchell <mazchoo@gmail.com>
2025-06-09 00:05:37 +0700

    Fixed stress kernel behaviour
    + Different result to Python but seems to have reasonable range

---
+++ profiling/kernels/apply_stress.cu
@@ -0,0 +1,65 @@
+__device__ __inline__ float3 subtract(float3 a, float3 b) {
+    return make_float3(a.x - b.x, a.y - b.y, a.z - b.z);
+}
+
+__device__ __inline__ float normL2(float3 v) {
+    return sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
+}
+
+__device__ __inline__ void scaleVector(float3 &v, float s) {
+    v.x *= s;
+    v.y *= s;
+    v.z *= s;
+}
+
+__global__ void apply_stress(float *acceleration, float *vertices,
+                             unsigned int* stress_relations, int nr_stress_relations) {
+    int pair_idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (pair_idx >= nr_stress_relations) return;
+    const float EPSILON = 1e-10f;
+
+    unsigned int from_ind = stress_relations[pair_idx * 2];
+    unsigned int to_ind = stress_relations[pair_idx * 2 + 1];
+
+    float3 from_vertex = make_float3(
+        vertices[from_ind * 3],
+        vertices[from_ind * 3 + 1],
+        vertices[from_ind * 3 + 2]
+    );
+    float3 to_vertex = make_float3(
+        vertices[to_ind * 3],
+        vertices[to_ind * 3 + 1],
+        vertices[to_ind * 3 + 2]
+    );
+
+    float3 vector = subtract(to_vertex, from_vertex);
+    float distance = normL2(vector);
+    float stress_amount = distance / STRESS_RESTING_AMOUNT;
+    if (stress_amount > 1 + STRESS_THRESHOLD) {
+        float3 vectorNorm = vector;
+        scaleVector(vectorNorm, 1 / stress_amount);
+        vector = subtract(vector, vectorNorm);
+        scaleVector(vector, STRESS_WEIGHTING);
+
+        acceleration[from_ind * 3] += vector.x;
+        acceleration[from_ind * 3 + 1] += vector.y;
+        acceleration[from_ind * 3 + 2] += vector.z;
+        acceleration[to_ind * 3] -= vector.x;
+        acceleration[to_ind * 3 + 1] -= vector.y;
+        acceleration[to_ind * 3 + 2] -= vector.z;
+    } else if (stress_amount < 1 - STRESS_THRESHOLD) {
+        if (stress_amount > EPSILON) {
+            float3 vectorNorm = vector;
+            scaleVector(vectorNorm, 1 / stress_amount);
+            vector = subtract(vector, vectorNorm);
+        }
+        scaleVector(vector, STRESS_WEIGHTING);
+
+        acceleration[from_ind * 3] -= vector.x;
+        acceleration[from_ind * 3 + 1] -= vector.y;
+        acceleration[from_ind * 3 + 2] -= vector.z;
+        acceleration[to_ind * 3] += vector.x;
+        acceleration[to_ind * 3 + 1] += vector.y;
+        acceleration[to_ind * 3 + 2] += vector.z;
+    }
+}
