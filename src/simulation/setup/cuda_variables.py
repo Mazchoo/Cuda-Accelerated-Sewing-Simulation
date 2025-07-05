dcee142a1db96b7f4669d7b361485b004be1e40a
Matthew Mitchell <mazchoo@gmail.com>
2025-07-05 14:26:48 +0700

    Made container for cuda variables

---
+++ src/simulation/setup/cuda_variables.py
@@ -0,0 +1,37 @@
+''' Container for all cuda arrays '''
+from typing import NamedTuple
+
+import pycuda.driver as cuda
+import numpy as np
+import numpy.typing as npt
+
+
+class CudaVariable:
+    ''' Correspondence between cpu and gpu variable '''
+    def __init__(self, array: np.ndarray):
+        self.cpu = array
+        self.gpu = cuda.mem_alloc(array.nbytes)
+        cuda.memcpy_htod(self.gpu, array.flatten())
+        self.length = np.uint32(len(array))
+
+    def copy_from_gpu(self) -> np.ndarray:
+        ''' Get updated variable from GPU '''
+        cuda.memcpy_dtoh(self.cpu, self.gpu)
+        return self.cpu
+
+    def __len__(self) -> npt.uint32:
+        return self.length
+
+
+class CudaVariables(NamedTuple):
+    ''' Container for cpu and gpu versions of each variable '''
+    vertices: CudaVariable
+    velocities: CudaVariable
+    accelerations: CudaVariable
+    stress_indices: CudaVariable
+    shear_indices: CudaVariable
+    bend_indices: CudaVariable
+    sewing_indices: CudaVariable
+    triangles: CudaVariable
+    triangle_centers: CudaVariable
+    traingle_normals: CudaVariable
