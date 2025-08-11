be857c543c3d570ef8868c513d5a7aa8d77c17d7
Matthew Mitchell <mazchoo@gmail.com>
2025-08-11 11:42:01 +0700

    Added comments to Camera.py

--- src/qt/gl_helpers/camera.py
+++ src/qt/gl_helpers/camera.py
@@ -1,7 +1,7 @@
 ''' Container for 4by4 matrix representing camera projection '''
 from typing import Dict, Optional
 
-import pyrr
+from pyrr import matrix44
 import numpy as np
 
 from OpenGL.GL import glUniformMatrix4fv
@@ -11,9 +11,17 @@ from src.qt.gl_helpers.uniforms import bind_globals_to_object, get_global_object
 
 
 class Camera:
+    ''' Stores and recalculates a 4x4 perspective projection matrix and can store it on GPU '''
+
     __slots__ = 'fovy', 'aspect', 'near', 'far', 'object_id', 'projection_matrix', 'globals'
 
     def __init__(self, fovy: float, aspect: float, near: float, far: float, **globals: Dict[str, str]):
+        '''
+            fovy: The vertical field of view angle in degrees
+            aspect: The aspect ratio (width/height) of the viewport
+            near: The distance to the near clipping plane (z is mapped to the valid range [-1, 1])
+            far: The distance to the far clipping plane (z is mapped to the valid range [-1, 1])
+        '''
         self.fovy = fovy
         self.aspect = aspect
         self.near = near
@@ -32,13 +40,15 @@ class Camera:
         near = near or self.near
         far = far or self.far
 
-        self.projection_matrix = pyrr.matrix44.create_perspective_projection(
+        self.projection_matrix = matrix44.create_perspective_projection(
             fovy=fovy, aspect=aspect, near=near, far=far, dtype=np.float32
         )
 
     def set_all_globals(self, shader: int = None, var_name: str = None):
+        ''' Update all camera properties on the GPU '''
         glob_id = get_global_object_id(self, "object_id", shader, var_name)
         glUniformMatrix4fv(glob_id, 1, GL_FALSE, self.projection_matrix)
 
     def bind_global_variable_names(self, shader):
+        ''' Bind camera properties to uniform variable names in the shader '''
         bind_globals_to_object(self, shader)
